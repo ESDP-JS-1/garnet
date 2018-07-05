a686881f70cd86ad3dae246f6601e694d7c93b52
Vladislav Laptev <razzztaedd@gmail.com>
2018-07-05 19:31:46 +0600

    #10 create tree view, add component item node

--- front/src/containers/SkillsCategories/SkillsCategories.js
+++ front/src/containers/SkillsCategories/SkillsCategories.js
@@ -2,40 +2,56 @@ import React from 'react';
 import {fetchSkill, fetchSkillCategory} from "../../store/actions/skills";
 import {connect} from "react-redux";
 import LTT from "list-to-tree";
-import TreeView from "treeview-react-bootstrap";
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
+import NodeItem from "../../components/NodeItem/NodeItem";
 
 class SkillsCategories extends React.Component {
+  state = {
+    treeData: []
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchListCategory();
     this.props.fetchListSkills();
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.skillCategory !== this.props.skillCategory || prevProps.skillList !== this.props.skillList){
+      this.renderTree(this.props.skillCategory.concat(this.props.skillList));
+    }
+  }
+
 
   renderTree = (arr) => {
-      // console.log(arr);
-      const list = arr.map(value => {
+    const list = arr.map(value => {
       if (!value.parentId) {
         value.parentId = 0;
       }
-      value.text = value.title;
       return value;
     });
     const currentTree = new LTT(list, {
       key_id: '_id',
       key_parent: 'parentId',
-      key_child: 'nodes'
+      key_child: 'children'
     });
 
-    return currentTree.GetTree();
+    this.setState(prevState => {
+      return prevState.treeData = currentTree.GetTree()
+    });
   };
 
   render() {
-    const skillList = this.renderTree(this.props.skillCategory.concat(this.props.skillList));
-    return(
-      <TreeView data={skillList} />
+    return (
+      <div style={{ height: '400px' }}>
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={treeData => this.setState({ treeData })}
+          getNodeKey={({node}) => node._id}
+          nodeContentRenderer={NodeItem}
+        />
+      </div>
     )
-
   }
 }
 
