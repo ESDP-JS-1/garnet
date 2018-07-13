fd1fdbb351dcdafa030651eb427a79b17bf173fe
Egor Bugaev <bugaiyoff@gmail.com>
2018-07-13 19:21:57 +0600

    #25 edited run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 cd ../front/
+yarn
 pm2 start "yarn start:test" --name front
 
 cd ../api
+yarn
 pm2 start "yarn dev:test" --name api
 
 
 cd ../tests/
+yarn
 yarn start
 
 pm2 kill
